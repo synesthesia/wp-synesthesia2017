9c20a06d11dacbef65d7bdeb06beb758e015bb9f
Julian Elve <julian.elve@gmail.com>
2016-10-07 15:17:14 +0100

    add missing stylesheet link

--- header.php
+++ header.php
@@ -15,7 +15,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"
+<link rel="stylesheet" type="text/css" href="style.css">
 
 <?php wp_head(); ?>
 </head>
