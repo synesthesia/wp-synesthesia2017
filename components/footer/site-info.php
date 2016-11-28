e5f6806d7f124a75f88955b03d15b5a6f7d67d4c
Julian Elve <julian.elve@gmail.com>
2016-11-28 16:34:08 +0000

    layout and font changes

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'synesthesia2017' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'synesthesia2017' ), 'WordPress' ); ?></a>
-	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'synesthesia2017' ), 'Synesthesia2017B', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+	<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'synesthesia2017' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'synesthesia2017' ), 'WordPress' ); ?></a>-->
+	<!--span class="sep"> | </span>-->
+	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'synesthesia2017' ), 'Synesthesia2017', '<a href="http://synesthesia.co,uk/" rel="designer">Synesthesia</a>' ); ?>
 </div><!-- .site-info -->
